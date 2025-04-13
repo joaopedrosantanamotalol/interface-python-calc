3caf62c971dee5b815336a3e8087c25099ddf902
joaopedrosantanamotalol <jp1119174@gmail.com>
2025-04-12 23:20:17 -0300

    arrumei as funções e layout das telas

--- telas.py
+++ telas.py
@@ -5,14 +5,16 @@ def novaTela(janela):
     janela2.geometry("400x300")
     janela2.title("Janela 2")
     janela2.transient(janela)
+
+    titulo = e.CTkLabel(janela2, text="Vamos Somar", text_color="White", font=("Arial", 20, "bold"))
+    titulo.place(x=150, y=10)
     ipt1 = e.CTkEntry(janela2, placeholder_text="digite um numero")
-    ipt1.place(x=10, y=10)
-
+    ipt1.place(x=10, y=80)
     ipt2 = e.CTkEntry(janela2, placeholder_text="digite outro numero")
-    ipt2.place(x=10, y=40)
-
+    ipt2.place(x=10, y=130)
+    bntIr = e.CTkButton(janela2, text="Somar", command=lambda:clique()).place(x=10, y=190)
     mostrar = e.CTkLabel(janela2, text=" ")
-    mostrar.place(x=10, y=100)
+    mostrar.place(x=10, y=160)
 
     def clique():
         try:
@@ -22,10 +24,10 @@ def novaTela(janela):
             mostrar.configure(text=f"Erro: {ex}")
 
     btpegar = e.CTkButton(janela2, text="pegar", command=clique)
-    btpegar.place(x=10, y=70)
+    btpegar.place(x=10, y=190)
 
 
-    janela2.lift()
+    novaTela.lift()
 
 def JanelaTroll(janela):
     janelaT = e.CTkToplevel(janela, fg_color="darkblue")
@@ -34,14 +36,14 @@ def JanelaTroll(janela):
     janelaT.transient(janela)
 
     titulo = e.CTkLabel(janelaT, text="Vamos Multiplicar", text_color="White", font=("Arial", 20, "bold"))
-    titulo.place(x=100, y=10)
+    titulo.place(x=150, y=10)
     field = e.CTkEntry(janelaT, placeholder_text="digite um numero")
-    field.place(x=10, y=50)
+    field.place(x=10, y=80)
     field2 = e.CTkEntry(janelaT, placeholder_text="digite outro numero")
-    field2.place(x=10, y=80)
-    bntIr = e.CTkButton(janelaT, text="Multiplicar", command=lambda:clique()).place(x=10, y=160)
+    field2.place(x=10, y=130)
+    bntIr = e.CTkButton(janelaT, text="Multiplicar", command=lambda:clique()).place(x=10, y=190)
     mostrar = e.CTkLabel(janelaT, text=" ")
-    mostrar.place(x=10, y=130)
+    mostrar.place(x=10, y=160)
     
     def clique():
 
@@ -53,3 +55,32 @@ def JanelaTroll(janela):
 
 
     janelaT.lift()
+
+def janela3(janela):
+    janela3 = e.CTkToplevel(janela, fg_color="darkblue")
+    janela3.geometry("400x300")
+    janela3.title("subtração")
+    janela3.transient(janela)
+
+    titulo = e.CTkLabel(janela3, text="Vamos Subtrair", text_color="White", font=("Arial", 20, "bold"))
+    titulo.place(x=150, y=10)
+
+    ipt1 = e.CTkEntry(janela3, placeholder_text="digite um numero")
+    ipt1.place(x=10, y=80)
+
+    ipt2 = e.CTkEntry(janela3, placeholder_text="digite outro numero")
+    ipt2.place(x=10, y=130)
+
+    btpegar = e.CTkButton(janela3, text="pegar", command=clique).place(x=10, y=160)
+
+    mostrar = e.CTkLabel(janela3, text=" ")
+    mostrar.place(x=10, y=160)
+
+    def clique():
+        try:
+            resultado = equacoes.subtracao(ipt1.get(), ipt2.get())
+            mostrar.configure(text=f"Resultado: {resultado}", text_color="Lightgreen", font=("Arial", 20, "bold"))
+        except Exception as ex:
+            mostrar.configure(text=f"Erro: {ex}")
+
+    
